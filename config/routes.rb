b2ee8369587b692258ce5c28d33f285150c472f0
KraevRoot <Rosenroot@tut.by>
2016-09-16 14:39:36 +0300

    add comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
   root to: 'visitors#index'
   devise_for :users
 end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,6 +1,8 @@
 require 'support/helpers/session_helpers'
 require 'support/helpers/post_helpers'
+require 'support/helpers/comment_helpers'
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::PostHelpers, type: :feature
+  config.include Features::CommentHelpers, type: :feature
 end
