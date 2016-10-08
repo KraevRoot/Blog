80e536a2047b20b515eb206629a509ac64777845
KraevRoot <Rosenroot@tut.by>
2016-10-09 00:24:11 +0300

    add tests for categories

--- spec/features/categories/delete_categories_spec.rb
+++ spec/features/categories/delete_categories_spec.rb
@@ -10,4 +10,16 @@ feature 'Delete categories' do
     click_link ''
     expect(page).to have_content 'Category successfully deleted.'
   end
+
+  scenario 'user can\'t delete categories' do
+    user = FactoryGirl.create(:user)
+    signin(user.email, user.password)
+    visit admin_categories_path
+    expect(page).to have_content 'Forbidden'
+  end
+
+  scenario 'visitor can\'t delete categories' do
+    visit admin_categories_path
+    expect(page).to have_content 'Forbidden'
+  end
 end
